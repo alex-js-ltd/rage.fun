fac52f3c9496ced8c4735582b4acbf66032ea60e
alex-js-ltd <alex.js.ltd@gmail.com>
2025-09-13 11:20:57 +0200

    update

--- packages/rage/programs/rage/src/utils/seed.rs
+++ packages/rage/programs/rage/src/utils/seed.rs
@@ -3,6 +3,10 @@ pub const META_LIST_ACCOUNT_SEED: &str = "extra-account-metas";
 // Auth
 pub const BONDING_CURVE_AUTH_SEED: &str = "bonding_curve_auth";
 pub const TRADING_FEE_AUTH_SEED: &str = "trading_fee_auth";
+/// Authority PDA used to reconcile supply drift
+/// by minting offsets into the frozen sink account.
+/// These tokens are non-circulating and cannot be moved.
+pub const SYNC_AUTH_SEED: &str = "sync_auth";
 
 // State
 pub const BONDING_CURVE_STATE_SEED: &str = "bonding_curve_state";
