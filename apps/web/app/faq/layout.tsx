5acc515db08cc5911c88d30092462e1fe5f83390
alex-js-ltd <alex.js.ltd@gmail.com>
2025-09-26 13:02:56 +0100

    update

--- apps/web/app/faq/layout.tsx
+++ apps/web/app/faq/layout.tsx
@@ -16,6 +16,34 @@ export default function Layout({ children }: { children: ReactNode }) {
 				</div>
 				<div className="mx-auto flex max-w-screen-md flex-col items-center px-4 py-10 sm:px-6 sm:pt-20 lg:px-8">
 					<article className="prose-headings:font-display prose prose-gray max-w-none transition-all prose-headings:relative prose-headings:scroll-mt-20 prose-headings:font-semibold pb-64">
+						<table>
+							<thead>
+								<tr>
+									<th>Platform</th>
+									<th>Creator fee</th>
+									<th>Protocol fee</th>
+									<th>LP fee</th>
+									<th>Total fee</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>pump.fun</td>
+									<td>0.30%</td>
+									<td>0.95%</td>
+									<td>0%</td>
+									<td>1.25%</td>
+								</tr>
+								<tr>
+									<td>letsrage.fun</td>
+									<td>1.00%</td>
+									<td>0%</td>
+									<td>0%</td>
+									<td>1.00%</td>
+								</tr>
+							</tbody>
+						</table>
+
 						{children}
 					</article>
 					<div className="mt-10 w-full border-t border-zinc-200 pt-10 text-center">
