88ecfba58ee3466ee2006d475b8e016b2757a402
alex-js-ltd <alex.js.ltd@gmail.com>
2025-09-27 15:50:12 +0100

    update

--- apps/web/app/comps/footer.tsx
+++ apps/web/app/comps/footer.tsx
@@ -6,8 +6,8 @@ export function Footer() {
 	return (
 		<footer className="px-4">
 			<nav aria-label="Footer Navigation">
-				<ul className="flex justify-center items-center gap-4">
-					<li>
+				<ul className="flex  items-center gap-4">
+					<li className="flex-1">
 						<Link scroll={true} href="/faq" className="hover:underline transition-colors text-sm text-neutral-400">
 							FAQ
 						</Link>
@@ -17,7 +17,7 @@ export function Footer() {
 						<div className="w-[1px] h-3 bg-neutral-400" />
 					</li>
 
-					<li>
+					<li className="flex-1">
 						<a
 							href="https://discord.gg/FfmuN25GjE"
 							target="_blank"
@@ -32,7 +32,7 @@ export function Footer() {
 						<div className="w-[1px] h-3 bg-neutral-400" />
 					</li>
 
-					<li>
+					<li className="flex-1">
 						<a
 							href="https://t.me/+oHFpt8HM6EAyODFk"
 							target="_blank"
@@ -47,7 +47,7 @@ export function Footer() {
 						<div className="w-[1px] h-3 bg-neutral-400" />
 					</li>
 
-					<li>
+					<li className="flex-1">
 						<a
 							href="https://discord.gg/FfmuN25GjE"
 							target="_blank"
