39a14b6e163712ab4341ebbeb8fb375c5d0e5358
alex-js-ltd <alex.js.ltd@gmail.com>
2025-09-27 13:59:37 +0100

    update

--- apps/web/app/comps/swap_form.tsx
+++ apps/web/app/comps/swap_form.tsx
@@ -433,6 +433,7 @@ function QuickOptions({
 					<Button
 						type="button"
 						onClick={() => {
+							if (o.uiAmount === '') return
 							control.change(o.uiAmount)
 						}}
 						variant="interval"
