c0890dcdff48d5f827568abe6acc64e53b52c5d9
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-05 13:38:28 +0100

    update

--- apps/web/app/(app)/[id]/page.tsx
+++ apps/web/app/(app)/[id]/page.tsx
@@ -5,7 +5,7 @@ import { type SearchParams } from '@/app/utils/schemas'
 import { getTokens } from '@/app/data/get_tokens'
 import { TokenGrid, TokenGridFallback } from '@/app/comps/token_grid'
 
-import { Back } from '@/app/comps/back'
+import { Header } from '@/app/comps/header'
 
 export const dynamic = 'force-dynamic'
 
@@ -24,16 +24,10 @@ export default async function Page(props: Props) {
 	const tokenPromise = getTokens({ sortType, sortOrder, cursorId, creatorId })
 
 	return (
-		<div className="flex-1">
-			<div
-				className="sticky top-0 h-[52px] border-x border-white border-opacity-[0.125] flex items-center z-40 w-full
-                  bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60
-                  border-b border-white/10 max-w-[600px] frost "
-			>
-				<Back />
-			</div>
+		<div className="flex-1 border-x border-white border-opacity-[0.125]">
+			<Header></Header>
 
-			<div className="relative mx-auto flex max-w-[600px] flex-col pb-0 border-x border-white border-opacity-[0.125] min-h-[calc(100vh-52px)]">
+			<div className="relative mx-auto flex max-w-[600px] flex-col pb-0 border-t border-white border-opacity-[0.125] min-h-[calc(100vh-52px)]">
 				<section className="p-0 ">
 					<Suspense
 						fallback={
