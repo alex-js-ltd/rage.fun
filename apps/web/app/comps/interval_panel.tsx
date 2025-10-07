59533552326357bcf391eafc97c3bf6b4f21f250
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-07 20:31:37 +0100

    update

--- apps/web/app/comps/interval_panel.tsx
+++ apps/web/app/comps/interval_panel.tsx
@@ -41,8 +41,8 @@ export function IntervalPanel({ mint, searchParams }: { mint: string; searchPara
 						position="popper"
 						side="bottom"
 						align="start"
-						sideOffset={24}
-						className="relative w-[--radix-select-trigger-width] min-w-[--radix-select-trigger-width] overflow-hidden bg-background-100  z-10  h-auto data-[state=open]:animate-scale-in-95 data-[state=closed]:animate-scale-out-50 "
+						sideOffset={18}
+						className="relative w-[--radix-select-trigger-width] min-w-[--radix-select-trigger-width] overflow-hidden bg-background-100  z-10  h-auto data-[state=open]:animate-scale-in-95 data-[state=closed]:animate-scale-out-50  "
 					>
 						<SelectPrimitive.Viewport>
 							<SelectPrimitive.Group>
