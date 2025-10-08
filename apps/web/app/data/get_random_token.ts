ebac7b07ee178386e206d1c5223eb11dc7c6e32f
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-08 10:21:16 +0100

    update

--- apps/web/app/data/get_random_token.ts
+++ apps/web/app/data/get_random_token.ts
@@ -2,11 +2,21 @@ import { prisma } from '@/app/utils/db'
 import { Prisma } from '@prisma/client'
 
 export async function getRandomToken() {
-	const latest = await prisma.token.findMany({
-		orderBy: { createdAt: 'desc' },
-		take: 10,
-		select: { id: true, createdAt: true }, // add fields you need
+	const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000)
+
+	const total = await prisma.token.count({
+		where: { createdAt: { lt: fiveMinutesAgo } },
 	})
 
-	return latest[Math.floor(Math.random() * latest.length)]
+	if (total === 0) throw new Error('No eligible tokens')
+
+	const randomIndex = Math.floor(Math.random() * total)
+
+	const token = await prisma.token.findFirstOrThrow({
+		where: { createdAt: { lt: fiveMinutesAgo } },
+		skip: randomIndex,
+		select: { id: true, createdAt: true }, // add fields as needed
+	})
+
+	return token
 }
