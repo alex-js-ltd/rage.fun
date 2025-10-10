d38954962540d325264866ea353d74038a91bca7
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-10 14:02:54 +0100

    update

--- apps/web/app/data/get_trending_tokens.ts
+++ apps/web/app/data/get_trending_tokens.ts
@@ -4,7 +4,7 @@ import 'server-only'
 
 export async function getTrendingTokens(): Promise<TokenFeedType[]> {
 	const res = await kv.get<string>('trending_tokens')
-
+	console.log(res)
 	if (!res) {
 		return []
 	}
