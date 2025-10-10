5b560cb158272580359f274589f56cf43534cc96
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-10 18:02:20 +0100

    update

--- apps/web/app/(app)/@right/earn/page.tsx
+++ apps/web/app/(app)/@right/earn/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import { SearchField, SearchResults } from '@/app/comps/search'
 import { searchTokens } from '@/app/data/search_tokens'
 import { type SearchParams } from '@/app/utils/schemas'
+import { getTrendingTokens } from '@/app/data/get_trending_tokens'
+import { Trending } from '@/app/comps/trending'
 
 export const dynamic = 'force-dynamic'
 
@@ -16,15 +18,23 @@ export default async function Page(props: Props) {
 
 	const searchPromise = searchTokens(search)
 
+	const trendingPromise = getTrendingTokens()
+
 	return (
 		<div className="relative w-full">
 			<div className="sticky top-0 z-40 flex flex-col">
-				<div className="flex h-[52px] items-center">
+				<div className="mt-[6px]">
 					<SearchField />
 				</div>
 
 				<Suspense>
 					<SearchResults searchPromise={searchPromise} />
+				</Suspense>
+
+				<div className="h-[2px] bg-white/[0.125] my-4" />
+
+				<Suspense>
+					<Trending trendingPromise={trendingPromise} />
 				</Suspense>
 			</div>
 		</div>
