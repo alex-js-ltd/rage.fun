4d5248f79e687c172cd6fcbeb05f64c9381bdddb
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-13 17:16:08 +0100

    update

--- apps/web/app/data/get_random_token.ts
+++ apps/web/app/data/get_random_token.ts
@@ -16,8 +16,6 @@ export async function getRandomToken() {
 		where: { createdAt: { lt: fiveMinutesAgo } },
 		skip: randomIndex,
 		select: { id: true, createdAt: true }, // add fields as needed
-
-		take: 5,
 	})
 
 	return token
