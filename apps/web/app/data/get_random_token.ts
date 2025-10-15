ba90f6d18e31fc3341aef419839b0f2a4df009b0
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-15 11:16:14 +0100

    update

--- apps/web/app/data/get_random_token.ts
+++ apps/web/app/data/get_random_token.ts
@@ -3,14 +3,23 @@ import { Prisma } from '@prisma/client'
 import dayjs from 'dayjs'
 import 'server-only'
 
-export async function getRandomToken() {
+export async function getRandomToken(notIn?: boolean) {
 	const since = dayjs().subtract(5, 'minute').toDate()
 
+	const exclude = notIn
+		? [
+				'795Z4uFBZxK8c5uYwMKrMqNyEs4VXCZJ4DFRFJt6qH18',
+				'EbAo94MQ8YhKLiihQDsN6QD7zMm8SDKUbdbaD5ijPu8N',
+				'EbAo94MQ8YhKLiihQDsN6QD7zMm8SDKUbdbaD5ijPu8N',
+			]
+		: []
+
 	const latest = await prisma.token.findMany({
-		where: { createdAt: { lt: since } },
+		where: { createdAt: { lt: since }, creatorId: { notIn: exclude } },
 		orderBy: { createdAt: 'desc' },
-		take: 10,
-		select: { id: true, createdAt: true }, // add other fields if needed
+
+		select: { id: true, createdAt: true },
+		take: 5,
 	})
 
 	const randomIndex = Math.floor(Math.random() * latest.length)
