ec63277fea4369078560ff8f26722874991b46c3
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-18 18:53:14 +0100

    update

--- apps/web/app/data/get_transaction_data.ts
+++ apps/web/app/data/get_transaction_data.ts
@@ -2,11 +2,15 @@ import { type TransactionTableType, createTransactionTableSchema } from '@/app/u
 import { getSolPrice } from '@/app/data/get_sol_price'
 import { getSwapEvents } from '@/app/data/get_swap_events'
 import { getDecimals } from '@/app/data/get_decimals'
+import dayjs from 'dayjs'
 import 'server-only'
 
 export async function getTransactionData(mint: string) {
 	const [swapEvents, decimals, solPrice] = await Promise.all([getSwapEvents(mint), getDecimals(mint), getSolPrice()])
 
+	swapEvents.sort(
+		(a, b) => dayjs.unix(Number(b?.time.toString())).valueOf() - dayjs.unix(Number(a?.time.toString())).valueOf(),
+	)
 	const TransactionTableSchema = createTransactionTableSchema({ decimals, solPrice })
 
 	const transactionData = swapEvents.reduce<TransactionTableType[]>((acc, curr) => {
