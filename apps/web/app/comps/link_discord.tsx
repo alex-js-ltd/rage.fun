b9d1893f77355ee8a0b371d5f418757dd80fa11b
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-26 23:06:09 +0000

    update

--- apps/web/app/comps/link_discord.tsx
+++ apps/web/app/comps/link_discord.tsx
@@ -4,8 +4,9 @@ import { signIn, useSession } from 'next-auth/react'
 import { useAsync } from '@/app/hooks/use_async'
 import { type ButtonProps, Button } from './button'
 import { storeSession } from '@/app/actions/store_session'
+import Link from 'next/link'
 
-export function LinkDiscord({ onClick, children, ...rest }: ButtonProps) {
+export function LinkDiscord({ onClick, children, discordId, ...rest }: ButtonProps & { discordId: string | null }) {
 	const session = useSession()
 
 	console.log('session', session)
@@ -16,6 +17,14 @@ export function LinkDiscord({ onClick, children, ...rest }: ButtonProps) {
 		await signIn('discord', {
 			callbackUrl: `/api/link_discord?userId=${userId}`,
 		})
+	}
+
+	if (discordId) {
+		return (
+			<a href="https://discord.gg/FfmuN25GjE" target="_blank" rel="noopener noreferrer">
+				{children}
+			</a>
+		)
 	}
 
 	return (
