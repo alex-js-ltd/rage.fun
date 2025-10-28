b117caa2ec261493f52de5f9dec07dcf89c80037
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-28 13:05:53 +0000

    update

--- apps/web/app/data/get_candlestick_data.ts
+++ apps/web/app/data/get_candlestick_data.ts
@@ -96,3 +96,70 @@ export async function getCandlstickData(mint: string, interval: number) {
 
 	return { data, markers }
 }
+
+let randomFactor = 25 + Math.random() * 25
+const samplePoint = (i: number) =>
+	i *
+		(0.5 + Math.sin(i / 1) * 0.2 + Math.sin(i / 2) * 0.4 + Math.sin(i / randomFactor) * 0.8 + Math.sin(i / 50) * 0.5) +
+	200 +
+	i * 2
+
+function generateData(numberOfCandles = 500, updatesPerCandle = 5, startAt = 100) {
+	const createCandle = (val: number, time: UTCTimestamp) => ({
+		time,
+		open: val,
+		high: val,
+		low: val,
+		close: val,
+	})
+
+	const updateCandle = (candle: CandlestickData, val: number) => ({
+		time: candle.time,
+		close: val,
+		open: candle.open,
+		low: Math.min(candle.low, val),
+		high: Math.max(candle.high, val),
+	})
+
+	randomFactor = 25 + Math.random() * 25
+	const date = new Date(Date.UTC(2018, 0, 1, 12, 0, 0, 0))
+
+	const numberOfPoints = numberOfCandles * updatesPerCandle
+
+	const initialData = []
+	const realtimeUpdates = []
+
+	let lastCandle
+	let previousValue = samplePoint(-1)
+
+	for (let i = 0; i < numberOfPoints; ++i) {
+		if (i % updatesPerCandle === 0) {
+			date.setUTCDate(date.getUTCDate() + 1)
+		}
+		const time = (date.getTime() / 1000) as UTCTimestamp
+		let value = samplePoint(i)
+		const diff = (value - previousValue) * Math.random()
+		value = previousValue + diff
+		previousValue = value
+		if (i % updatesPerCandle === 0) {
+			const candle = createCandle(value, time)
+			lastCandle = candle
+			if (i >= startAt) {
+				realtimeUpdates.push(candle)
+			}
+		} else {
+			const newCandle: CandlestickData | undefined = lastCandle && updateCandle(lastCandle, value)
+			lastCandle = newCandle
+			if (i >= startAt) {
+				realtimeUpdates.push(newCandle)
+			} else if ((i + 1) % updatesPerCandle === 0) {
+				initialData.push(newCandle)
+			}
+		}
+	}
+
+	return {
+		initialData,
+		realtimeUpdates,
+	}
+}
