ed3cb8962041fa98eb7fc6c5e98b07be47c40464
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-28 20:18:48 +0000

    update

--- apps/web/app/discord/page.tsx
+++ apps/web/app/discord/page.tsx
@@ -4,10 +4,10 @@ import { Redirect } from '@/app/comps/redirect'
 
 export const metadata: Metadata = {
 	title: 'letsrage.fun on Discord',
-	description: 'Launch a token and trade with zero extraction.',
+	description: 'Launch a token and trade with ZERO extraction.',
 	openGraph: {
 		title: 'letsrage.fun on Discord',
-		description: 'Launch a token and trade with zero extraction.',
+		description: 'Launch a token and trade with ZERO extraction.',
 		images: [
 			{
 				url: 'https://letsrage.fun/rage.png',
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 	twitter: {
 		card: 'summary_large_image',
 		title: 'letsrage.fun on Discord',
-		description: 'Launch a token and trade with zero extraction.',
+		description: 'Launch a token and trade with ZERO extraction.',
 		images: ['https://letsrage.fun/rage.png'],
 	},
 }
