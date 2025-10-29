5feae3889b6739593c5c51f5319ce8b50e2c59d5
alex-js-ltd <alex.js.ltd@gmail.com>
2025-10-29 11:15:24 +0000

    update

--- apps/web/app/data/get_top_creators.ts
+++ apps/web/app/data/get_top_creators.ts
@@ -5,7 +5,7 @@ import 'server-only'
 
 export async function getTopCreators(limit = 10) {
 	const aggregated = await prisma.harvestEvent.groupBy({
-		by: ['signer'], // group by wallet address
+		by: ['signer'],
 		_sum: {
 			lamports: true,
 		},
@@ -21,30 +21,13 @@ export async function getTopCreators(limit = 10) {
 		aggregated.map(async row => {
 			const user = await prisma.user.findUnique({
 				where: { id: row.signer },
-				select: {
-					id: true,
-					createdAt: true,
-					tokens: {
-						select: {
-							id: true,
-							metadata: {
-								select: {
-									name: true,
-									symbol: true,
-									image: true,
-								},
-							},
-						},
-					},
-				},
 			})
 
 			const totalYieldSol = new Decimal(row._sum.lamports?.toString() ?? 0).div(LAMPORTS_PER_SOL).toNumber()
 
 			return {
-				signer: row.signer,
-				totalYieldSol,
 				user,
+				yield: totalYieldSol,
 			}
 		}),
 	)
