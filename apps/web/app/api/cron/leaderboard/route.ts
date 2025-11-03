9d910c34ca96289c62c0ab95bdce6b16a8cbec2c
alex-js-ltd <alex.js.ltd@gmail.com>
2025-11-03 12:26:26 +0000

    update

--- apps/web/app/api/cron/leaderboard/route.ts
+++ apps/web/app/api/cron/leaderboard/route.ts
@@ -12,14 +12,11 @@ export async function GET(req: NextRequest) {
 	const requestHeaders = new Headers(req.headers)
 	const authorization = requestHeaders.get('authorization')
 
-	console.log('authorization', authorization)
-	console.log('CRON_SECRET', CRON_SECRET)
-
 	if (authorization !== `Bearer ${CRON_SECRET}`) {
 		return NextResponse.json('💩', { status: 401 })
 	}
 
-	const leaderBoard = await getLeaderBoard()
+	const leaderBoard = await getLeaderBoard(5)
 
 	await kv.set('leader_board', leaderBoard)
 
