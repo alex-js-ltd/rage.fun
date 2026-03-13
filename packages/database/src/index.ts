e5dfe9babe3fac68576ac20e1866a05792041bf4
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-13 10:50:53 +0000

    update

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,3 +1,4 @@
 export { prisma } from './client' // exports instance of prisma
 export * from '../generated/prisma/client' // exports generated types from prisma
 export * from './selects'
+export * from './payloads'
---
+++ packages/database/src/payloads/index.ts
@@ -0,0 +1,4 @@
+export type { TokenFeedRow } from './token_feed'
+export type { SwapConfigRow } from './swap_config'
+export type { SwapEventRow } from './swap_events'
+export type { TokenPnlRow } from './token_pnl'
