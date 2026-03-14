2467201f2b6c44c8472e2bb720b8e5d637940329
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-14 14:23:13 +0000

    update

--- apps/web/app/(app)/home/page.tsx
+++ apps/web/app/(app)/home/page.tsx
@@ -1,1 +1,28 @@
-export { default } from '@/app/feature/token_feed'
+import { Suspense } from 'react'
+import { SearchParamsSchema } from '@/app/utils/schemas'
+import type { SearchParams } from '@/app/utils/schemas'
+import { getTokenFeed } from '@/app/data/get_token_feed'
+import { notFound } from 'next/navigation'
+
+type Props = {
+	searchParams: Promise<SearchParams>
+}
+
+export default async function Page(props: Props) {
+	const searchParams = await props.searchParams
+
+	const submission = SearchParamsSchema.safeParse(searchParams)
+
+	if (submission.error) {
+		console.error(submission)
+		notFound()
+	}
+
+	const { sortType, sortOrder } = submission.data
+
+	const tokenFeedPromise = getTokenFeed({ sortOrder, sortType })
+
+	console.log(await tokenFeedPromise)
+
+	return <div className="w-full max-w-[600px] border-white border-x border-opacity-[0.125] bg-background-100"></div>
+}
