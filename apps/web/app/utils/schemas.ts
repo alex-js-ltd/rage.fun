2480046de3e9f80f3a004e5e707c3ba9b0ec9c1d
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-14 14:20:51 +0000

    update

---
+++ apps/web/app/utils/schemas.ts
@@ -0,0 +1,13 @@
+import type { Intent } from '@conform-to/react'
+import { conformZodMessage } from '@conform-to/zod'
+import { z } from 'zod'
+
+export const SearchParamsSchema = z.object({
+	sortType: z.enum(['createdAt', 'lastTrade', 'marketCap', 'volume']).default('createdAt'),
+	sortOrder: z.enum(['asc', 'desc']).default('desc'),
+	cursorId: z.string().optional(),
+	search: z.string().optional(),
+	creatorId: z.string().optional(),
+})
+
+export type SearchParams = z.infer<typeof SearchParamsSchema>
