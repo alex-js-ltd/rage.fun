d4a9e3cd5d8e353535a0966848e629cd461a4ae7
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-14 14:21:59 +0000

    update

---
+++ apps/web/app/data/get_sol_price.ts
@@ -0,0 +1,10 @@
+import { kv } from '@vercel/kv'
+
+export async function getSolPrice(): Promise<number> {
+	const res = await kv.get<{ usd: number }>(`sol_price`)
+
+	if (!res) {
+		throw new Error('failed to fetch sol price')
+	}
+	return res.usd
+}
