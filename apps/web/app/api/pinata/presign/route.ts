833ea3243e9d3bad3796e0f0526ed43bbf55300d
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-15 10:44:35 +0000

    updaate

---
+++ apps/web/app/api/pinata/presign/route.ts
@@ -0,0 +1,33 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import { PinataSDK } from 'pinata'
+import { getServerEnv } from '@/app/utils/env'
+
+const { PINATA_JWT } = getServerEnv()
+
+export const dynamic = 'force-dynamic'
+
+const pinata = new PinataSDK({
+	pinataJwt: PINATA_JWT,
+	pinataGateway: 'indigo-adverse-vicuna-777.mypinata.cloud',
+})
+
+export async function GET(req: NextRequest) {
+	console.log('--- /api/pinata/presign ---')
+	console.log('method:', req.method)
+	console.log('origin:', req.headers.get('origin'))
+	console.log('user-agent:', req.headers.get('user-agent'))
+	console.log('cookies:', req.headers.get('cookie'))
+
+	// Handle your auth here to protect the endpoint
+	try {
+		const url = await pinata.upload.public.createSignedURL({
+			expires: 30, // The only required param
+			mimeTypes: ['image/*', 'image/gif'],
+			maxFileSize: 5000000, // Optional file size limit
+		})
+		return NextResponse.json({ url }, { status: 200 }) // Returns the signed upload URL
+	} catch (error) {
+		console.log(error)
+		return NextResponse.json({ text: 'Error creating signed URL:' }, { status: 500 })
+	}
+}
--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,69 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
 
-export default nextConfig;
+	images: {
+		formats: ['image/webp'],
+
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'img-v1.raydium.io',
+				port: '',
+				pathname: '/icon/**',
+			},
+
+			{
+				protocol: 'https',
+				hostname: 'vn5d3lsmmr2noe9p.public.blob.vercel-storage.com',
+				port: '',
+				pathname: '/**',
+			},
+
+			{
+				protocol: 'https',
+				hostname: 'vqcczuygjg63d4lo.public.blob.vercel-storage.com',
+				port: '',
+				pathname: '/**',
+			},
+
+			{
+				protocol: 'https', // Add the protocol for loremflickr
+				hostname: 'loremflickr.com', // Add the hostname for loremflickr
+				port: '',
+				pathname: '/**', // Allow all paths under this hostname
+			},
+
+			{
+				protocol: 'https',
+				hostname: 'picsum.photos', // Add picsum.photos
+				port: '',
+				pathname: '/**', // Allow all paths under this domain
+			},
+
+			{
+				protocol: 'https',
+				hostname: 'indigo-adverse-vicuna-777.mypinata.cloud', // Add picsum.photos
+				port: '',
+				pathname: '/**', // Allow all paths under this domain
+			},
+
+			{
+				protocol: 'https',
+				hostname: 'violet-giant-deer-1.mypinata.cloud', // Add picsum.photos
+				port: '',
+				pathname: '/**', // Allow all paths under this domain
+			},
+
+			{
+				protocol: 'https',
+				hostname: 'cdn.discordapp.com', // Add picsum.photos
+				port: '',
+				pathname: '/**', // Allow all paths under this domain
+			},
+		],
+	},
+}
+
+export default nextConfig
