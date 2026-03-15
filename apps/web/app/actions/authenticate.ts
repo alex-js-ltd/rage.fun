c65572e76797d6c3452232009998a49e70e11035
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-15 10:22:07 +0000

    update

---
+++ apps/web/app/actions/authenticate.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import { signIn, signOut } from '@/app/auth'
+import { AuthError } from 'next-auth'
+import { parseWithZod } from '@conform-to/zod'
+import { AuthSchema } from '@/app/utils/schemas'
+
+export async function authenticate(_prevState: unknown, formData: FormData) {
+	const submission = parseWithZod(formData, {
+		schema: AuthSchema,
+	})
+
+	if (submission.status !== 'success') {
+		return {
+			...submission.reply(),
+		}
+	}
+
+	try {
+		await signIn('credentials', { ...submission.value, redirect: true })
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return 'Invalid credentials.'
+				default:
+					return 'Something went wrong.'
+			}
+		}
+		throw error
+	}
+}
+
+export async function disconnect() {
+	await signOut({ redirect: true })
+}
