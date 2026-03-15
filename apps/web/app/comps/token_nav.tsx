8453d445056f0ad28b0af0f6da522c6fd7055d3f
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-15 11:46:18 +0000

    update

---
+++ apps/web/app/comps/token_nav.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { cn } from '@/app/utils/misc'
+import { NavLink } from '@/app/comps/ui/nav_link'
+
+export function ExploreNav({ searchParams }: { searchParams: { sortType: string } }) {
+	const { sortType } = searchParams
+	return (
+		<div className="flex items-center w-full h-full overflow-x-scroll scrollbar-hide">
+			<NavLink
+				className={cn(
+					'flex-1 relative h-full w-fit flex items-center justify-center px-4 hover:bg-white/10 whitespace-nowrap',
+				)}
+				href={{
+					pathname: `/home`,
+					query: { sortType: 'createdAt' },
+				}}
+				as={`/home?sortType=createdAt`}
+				replace
+				prefetch={false}
+			>
+				{({ isActive }) => (
+					<div className="relative w-fit h-full flex items-center">
+						<span className={cn('font-medium text-text-200 text-[15px]', sortType === 'createdAt' && 'text-white')}>
+							Created At
+						</span>
+
+						<div
+							className={cn(
+								'absolute bottom-0 left-0 right-0 h-[1px]',
+								sortType === 'createdAt' && 'border-2 border-rage-100 rounded',
+							)}
+						/>
+					</div>
+				)}
+			</NavLink>
+
+			<NavLink
+				className={cn(
+					'flex-1 relative h-full w-fit flex items-center justify-center px-4 hover:bg-white/10 whitespace-nowrap',
+				)}
+				href={{
+					pathname: `/home`,
+					query: {
+						sortType: 'lastTrade',
+					},
+				}}
+				as={`/home?sortType=lastTrade`}
+				replace
+				prefetch={false}
+			>
+				{({ isActive }) => (
+					<div className="relative w-fit h-full flex items-center">
+						<span className={cn('font-medium text-text-200 text-[15px]', sortType === 'lastTrade' && 'text-white')}>
+							Last Trade
+						</span>
+
+						<div
+							className={cn(
+								'absolute bottom-0 left-0 right-0 h-[1px]',
+								sortType === 'lastTrade' && 'border-2 border-rage-100 rounded',
+							)}
+						/>
+					</div>
+				)}
+			</NavLink>
+
+			<NavLink
+				className={cn(
+					'flex-1 relative h-full w-fit flex items-center justify-center px-4 hover:bg-white/10 whitespace-nowrap ',
+				)}
+				href={{
+					pathname: `/home`,
+					query: {
+						sortType: 'marketCap',
+					},
+				}}
+				as={`/home?sortType=marketCap`}
+				replace
+				prefetch={false}
+			>
+				{({ isActive }) => (
+					<div className="relative w-fit h-full flex items-center">
+						<span className={cn('font-medium text-text-200 text-[15px]', sortType === 'marketCap' && 'text-white')}>
+							Market Cap
+						</span>
+
+						<div
+							className={cn(
+								'absolute bottom-0 left-0 right-0 h-[1px]',
+								sortType === 'marketCap' && 'border-2 border-rage-100 rounded',
+							)}
+						/>
+					</div>
+				)}
+			</NavLink>
+		</div>
+	)
+}
---
+++ apps/web/app/comps/ui/nav_link.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Link, { type LinkProps } from 'next/link'
+import { usePathname, useSearchParams } from 'next/navigation'
+
+export type NavLinkRenderProps = {
+	isActive: boolean
+}
+
+export interface NavLinkProps extends Omit<LinkProps, 'className' | 'style' | 'children'> {
+	children?: React.ReactNode | ((props: NavLinkRenderProps) => React.ReactNode)
+	className?: string
+}
+
+export function NavLink({ href, children, className, ...props }: NavLinkProps) {
+	const pathname = usePathname()
+	const searchParams = useSearchParams()
+
+	const url = searchParams.size === 0 ? pathname : `${pathname}?${searchParams}`
+
+	const isActive = url.startsWith(props.as as string)
+
+	let renderProps = {
+		isActive,
+	}
+
+	return (
+		<Link className={className} href={href} {...props}>
+			{typeof children === 'function' ? children(renderProps) : children}
+		</Link>
+	)
+}
