fefc3a2c66211c2f531544212153bc7413836dba
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-16 13:17:33 +0000

    update

--- apps/web/app/(app)/@modal/(.)create/modal.tsx
+++ apps/web/app/(app)/@modal/(.)create/modal.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import React, { type ReactNode, useRef, useEffect } from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { useRouter } from 'next/navigation'
 
