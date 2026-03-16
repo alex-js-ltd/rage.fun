eda8911a1ae92b5a0d8fb643de04b883c90b4f96
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-16 15:22:18 +0000

    update

--- apps/web/app/api/load_more/route.ts
+++ apps/web/app/api/load_more/route.ts
@@ -1,20 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { SearchParamsSchema } from '@/app/utils/schemas'
-import { parseWithZod } from '@conform-to/zod'
 import { getTokenFeed } from '@/app/data/get_token_feed'
+
+import { parseSubmission, report } from '@conform-to/react/future'
 
 export async function GET(req: NextRequest) {
 	const searchParams = req.nextUrl.searchParams
 
-	const submission = parseWithZod(searchParams, {
-		schema: SearchParamsSchema,
-	})
+	const submission = parseSubmission(searchParams)
 
-	if (submission.status !== 'success') {
-		return NextResponse.json(submission.reply(), { status: 404 })
+	const result = SearchParamsSchema.safeParse(submission.payload)
+
+	if (!result.success) {
+		return NextResponse.json(
+			{
+				...report(submission, {
+					error: {
+						issues: result.error.issues,
+					},
+				}),
+			},
+			{ status: 404 },
+		)
 	}
 
-	const { sortType, sortOrder, cursorId, creatorId } = submission.value
+	const { sortType, sortOrder, cursorId, creatorId } = result.data
 
 	const data = await getTokenFeed({ sortOrder, sortType, cursorId, creatorId })
 
