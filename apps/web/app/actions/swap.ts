2bab519a6bb45eb0e910bcdbaf52c1b06d712368
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-17 11:04:08 +0000

    update

--- apps/web/app/actions/swap.ts
+++ apps/web/app/actions/swap.ts
@@ -80,3 +80,79 @@ export async function buy(_prevState: unknown, formData: FormData) {
 		requestId,
 	}
 }
+
+export async function sell(_prevState: unknown, formData: FormData) {
+	const requestId = crypto.randomUUID()
+	const session = await auth()
+
+	if (!session) {
+		return null
+	}
+
+	const submission = parseSubmission(formData)
+
+	const result = SwapSchema.safeParse(submission.payload)
+
+	if (!result.success) {
+		return {
+			...report(submission, {
+				error: {
+					issues: result.error.issues,
+				},
+			}),
+			serializedTx: undefined,
+			errMessage: undefined,
+			requestId,
+		}
+	}
+
+	const { mint, amount, decimals, payer } = result.data
+
+	const ix = await getSellTokenIx({
+		program,
+		payer,
+		mint,
+		uiAmount: amount,
+		decimals,
+		minOutput: new BN(0),
+	})
+
+	const tx = await buildTransaction({
+		connection,
+		payer,
+		instructions: [ix],
+		signers: [],
+	})
+
+	const sim = await connection.simulateTransaction(tx)
+
+	if (sim.value.err !== null && !isInstructionError(sim.value.err)) {
+		return {
+			...report(submission, {
+				reset: true,
+			}),
+			serializedTx: undefined,
+			errMessage: 'Unkown error',
+			requestId,
+		}
+	} else if (sim.value.err !== null && isInstructionError(sim.value.err)) {
+		const code = sim.value.err.InstructionError[1].Custom
+		const errMessage = getErrorMessage(code)
+
+		return {
+			...report(submission, {
+				reset: true,
+			}),
+			serializedTx: undefined,
+			errMessage,
+			requestId,
+		}
+	}
+
+	return {
+		...report(submission, { reset: true }),
+		serializedTx: tx.serialize(),
+		errMessage: undefined,
+		requestId,
+	}
+}
