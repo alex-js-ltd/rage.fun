2063555b7c72d44be73bcc88b1873026185b0477
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-17 09:49:14 +0000

    add pnl

---
+++ apps/web/app/data/get_token_pnl.ts
@@ -0,0 +1,38 @@
+import { prisma, selectTokenPnl as select } from '@repo/database'
+import type { TokenPnlRow } from '@repo/database'
+import { getSolPrice } from '@/app/data/get_sol_price'
+import Decimal from 'decimal.js'
+import { solToUsd } from '@/app/utils/misc'
+import 'server-only'
+
+export async function getTokenPnl(mint: string) {
+	const data = await prisma.tokenPnl.findMany({
+		where: {
+			tokenId: mint,
+
+			sold: {
+				gt: 0, // only include wallets that sold something
+			},
+		},
+		select,
+
+		orderBy: {
+			realizedPnl: 'desc', // 👈 biggest profit first
+		},
+	})
+
+	const solPrice = await getSolPrice()
+
+	return data.map(pnl => toPnl(pnl, solPrice))
+}
+
+export function toPnl(tokenPnl: TokenPnlRow, solPrice: number) {
+	const bought = solToUsd(new Decimal(tokenPnl.bought.toString()).div(1e9), solPrice).toNumber()
+	const sold = solToUsd(new Decimal(tokenPnl.sold.toString()).div(1e9), solPrice).toNumber()
+
+	const realizedPnl = solToUsd(new Decimal(tokenPnl.realizedPnl.toString()).div(1e9), solPrice).toNumber()
+
+	return { signer: tokenPnl.signer, tokenId: tokenPnl.tokenId, bought, sold, realizedPnl }
+}
+
+export type TokenPnl = ReturnType<typeof toPnl>
