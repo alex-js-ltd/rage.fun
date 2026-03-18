2e6311ff0634ad58a993c2531be039c93f4bd696
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-18 10:39:35 +0000

    update

--- apps/web/app/utils/dialect.ts
+++ apps/web/app/utils/dialect.ts
@@ -1,8 +1,3 @@
-export function generateSolanaBlink(mint: string): string {
-	const apiUrl = `https://www.letsrage.fun/api/dialect/buy?mint=${encodeURIComponent(mint)}`
-	return `https://dial.to/?action=${encodeURIComponent(`solana-action:${apiUrl}`)}`
-}
-
 export function buyBlink(mint: string): string {
 	const apiUrl = `https://www.letsrage.fun/api/dialect/buy?mint=${encodeURIComponent(mint)}`
 	return `https://dial.to/?action=${encodeURIComponent(`solana-action:${apiUrl}`)}`
