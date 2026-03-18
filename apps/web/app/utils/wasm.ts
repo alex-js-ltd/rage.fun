6c532bbb3b5791b8a283e43fd00b1e2c379e9856
alex-js-ltd <alex.js.ltd@gmail.com>
2026-03-18 09:06:55 +0000

    update

---
+++ apps/web/app/utils/wasm.ts
@@ -0,0 +1,27 @@
+import { WasmType } from '@/app/utils/schemas'
+import { client } from '@/app/utils/client'
+import { getEnv } from '@/app/utils/env'
+
+const { BASE_URL } = getEnv()
+
+export async function calculateBuyAmount(params: WasmType): Promise<string> {
+	return client<string>(`${BASE_URL}/api/wasm/calculate_buy_amount`, {
+		method: 'POST',
+		body: JSON.stringify(params),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		cache: 'no-store',
+	})
+}
+
+export async function calculateSellPrice(params: WasmType): Promise<string> {
+	return client<string>(`${BASE_URL}/api/wasm/calculate_sell_price`, {
+		method: 'POST',
+		body: JSON.stringify(params),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		cache: 'no-store',
+	})
+}
